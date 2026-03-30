f5737152f088b15fc388259e067b354bbe22968c
erick <franciscokauan2329@gmail.com>
2026-03-30 14:09:54 -0300

    estilização adicional

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    // Abrir Modal (ID corrigido para bater com o HTML)
+    $('#btnAnonimoOpen').on('click', function() {
+        $('#modalAnonimo').modal('show');
+    });
+
+    // Controle Sidebar Mobile
+    $('#openSidebar').on('click', function() {
+        $('#mainSidebar').addClass('show');
+        $('#overlay').addClass('show');
+    });
+
+    $('#closeSidebar, #overlay').on('click', function() {
+        $('#mainSidebar').removeClass('show');
+        $('#overlay').removeClass('show');
+    });
+
+    // Envio do Formulário com SweetAlert2
+    $('#formAnonimoAjax').on('submit', function(e) {
+        e.preventDefault();
+        const $btn = $(this).find('button');
+        $btn.html('<i class="fa-solid fa-spinner fa-spin"></i> Enviando...').prop('disabled', true);
+
+        setTimeout(() => {
+            $('#modalAnonimo').modal('hide');
+            Swal.fire({
+                title: 'Sucesso!',
+                text: 'Sua manifestação foi enviada. Protocolo: #DW-' + Math.floor(Math.random() * 9999),
+                icon: 'success',
+                confirmButtonColor: '#008542'
+            });
+            $btn.html('Enviar Agora').prop('disabled', false);
+            this.reset();
+        }, 1500);
+    });
+});
